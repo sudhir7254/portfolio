542fcdcedc86307e8311cd6011c0496057b29fe0
Sudhir Yadav <158850211+sudhir7254@users.noreply.github.com>
2024-03-21 02:10:13 +0530

    Add files via upload

---
+++ portfolio/animated-portfolio-main/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Full stack java Developer" , "MERN Developer" , "  "],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+
+
+
